9600c3f17e6985b3417ecae801e3fa9dee3eb787
Jomiel <enriquez.jliquigan@gmail.com>
2025-10-03 19:40:22 +0800

    update

---
+++ classroom-monitoring-system/classroom-monitoring-system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace classroom_monitoring_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
